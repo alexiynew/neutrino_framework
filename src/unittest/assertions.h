380358988ed5bb998c885ec789f386d1af8bf36e
alexiynew <lehafeda@gmail.com>
2016-07-26 23:17:54 +0300

    Fix test assertion check.

--- src/unittest/assertions.h
+++ src/unittest/assertions.h
@@ -6,5 +6,5 @@
     testFailed(__FILE__, __LINE__, (MSG) != 0 ? #MSG : "")
 
 #define TEST_ASSERT(EXPR, MSG) \
-    (EXPR) ? testFailed(__FILE__, __LINE__, (MSG) != 0 ? #MSG : "") : static_cast<void>(0)
+    !(EXPR) ? testFailed(__FILE__, __LINE__, (MSG) != 0 ? #MSG : "") : static_cast<void>(0)
 #endif
