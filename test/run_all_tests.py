d43eec87a55c61a2e947974e41fc8fedb5caaf0f
alexiynew <lehafeda@gmail.com>
2016-10-30 11:15:58 +0300

    Add elapsed time and colors to tests output.

--- test/run_all_tests.py
+++ test/run_all_tests.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+import getopt
 import os
+import sys
+import time
 from subprocess import Popen, PIPE
-import sys, getopt
 
-test_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'test')
+
+test_dir = os.path.dirname(os.path.realpath(__file__))
 folder_test_script = 'run.py'
 
 verbose = False
@@ -20,19 +23,36 @@ class Test:
         self.executable = executable
 
 
+class Colors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 def run_test(test: Test) -> bool:
+    elapsed = 0
     if os.path.isfile(test.executable):
+        start = time.time()
         process = Popen([test.executable], stdout=PIPE, stdin=PIPE, stderr=PIPE)
         test.log = process.communicate()[0].decode("utf-8")
+        end = time.time()
+        elapsed = end - start
         test.success = process.returncode == 0
     else:
         test.success = False
         test.log = "File {} not exists.".format(test.executable)
 
     if test.success:
-        print("{:<50} OK 0.0s".format(test.name))
+        print("{:<50} ".format(test.name) + Colors.OKGREEN + "OK" + Colors.ENDC + " {:.2f}s".format(elapsed))
     else:
-        print("{:<50} FAIL".format(test.name))
+        print("{:<50} ".format(test.name) + Colors.FAIL + "FAIL" + Colors.ENDC)
+
+    if not test.success or verbose:
         print(test.log)
 
     return test.success
@@ -44,6 +64,7 @@ def run_folder_test(test: Test) -> bool:
 
 
 def run_tests(test_list):
+    start = time.time()
     passed_count = 0
     for test_name in test_list:
         test_name = test_name.strip()
@@ -54,14 +75,19 @@ def run_tests(test_list):
         else:
             passed_count += run_test(test)
 
+    end = time.time()
+    elapsed = end - start
     print()
-    print('-' * 50)
-    print("    Passed {}/{}".format(passed_count, len(test_list)))
+    print('-' * 60)
+    passed_string = "    Passed {}/{}".format(passed_count, len(test_list))
+    print("{:<50} {:.2f}s".format(passed_string, elapsed))
 
 
 def get_tests(path):
     files = os.listdir(path)
     files.sort()
+    this_file = os.path.basename(__file__)
+    files = [elem for elem in files if elem != this_file]
     return files
 
 
