02f7806f4474d85931425f3132db462719262aa5
alexiynew <lehafeda@gmail.com>
2016-12-09 23:42:09 +0300

    Add subfolders to tests.

--- test/run_all_tests.py
+++ test/run_all_tests.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 import getopt
 import os
 import sys
@@ -12,17 +12,6 @@ folder_test_script = 'run.py'
 verbose = False
 
 
-class Test:
-    name = ''
-    executable = ''
-    log = ''
-    success = False
-
-    def __init__(self, name, executable):
-        self.name = name
-        self.executable = executable
-
-
 class Colors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
@@ -34,64 +23,97 @@ class Colors:
     UNDERLINE = '\033[4m'
 
 
-def run_test(test: Test) -> bool:
+class Test:
+    name = ''
+    executable = ''
+    log = ''
+    success = False
     elapsed = 0
-    if os.path.isfile(test.executable):
-        start = time.time()
-        process = Popen([test.executable], stdout=PIPE, stdin=PIPE, stderr=PIPE)
-        test.log = process.communicate()[0].decode("utf-8")
-        end = time.time()
-        elapsed = end - start
-        test.success = process.returncode == 0
-    else:
-        test.success = False
-        test.log = "File {} not exists.".format(test.executable)
 
-    if test.success:
-        print("{:<50} ".format(test.name) + Colors.OKGREEN + "OK" + Colors.ENDC + " {:.2f}s".format(elapsed))
-    else:
-        print("{:<50} ".format(test.name) + Colors.FAIL + "FAIL" + Colors.ENDC)
+    def __init__(self, name, executable):
+        self.name = name
+        self.executable = executable
 
-    if not test.success or verbose:
-        print(test.log)
+    def __str__(self):
+        return "Test\n name: {}, executable: {}".format(self.name, self.executable)
 
-    return test.success
+    def __repr__(self):
+        return "Test()"
 
-
-def run_folder_test(test: Test) -> bool:
-    test.executable = os.path.join(test.executable, folder_test_script)
-    return run_test(test)
+    def run(self):
+        elapsed = 0
+        if os.path.isfile(self.executable):
+            start = time.time()
+            process = Popen([self.executable], stdout=PIPE, stdin=PIPE, stderr=PIPE)
+            self.log = process.communicate()[0].decode("utf-8")
+            end = time.time()
+            self.elapsed = end - start
+            self.success = process.returncode == 0
+        else:
+            self.success = False
+            self.log = "File {} not exists.".format(self.executable)
 
 
 def run_tests(test_list):
+
     start = time.time()
-    passed_count = 0
-    test_count = len(test_list);
-    for test_name in test_list:
-        test_name = test_name.strip()
-        executable = os.path.join(test_dir, test_name.strip())
-        test = Test(test_name, executable)
-        if os.path.isdir(test.executable):
-            passed_count += run_folder_test(test)
-        else:
-            passed_count += run_test(test)
+    tests_count = 0;
+    passed_count = 0;
+    for pack in test_list:
+        print(pack)
+        print('-' * 60)
+        for test in test_list[pack]:
+
+            tests_count += 1
+
+            test.run()
+
+            if test.success:
+                print("    {:.<50} ".format(test.name + " ") + Colors.OKGREEN + "OK" + Colors.ENDC + " {:.2f}s".format(test.elapsed))
+            else:
+                print("    {:.<50} ".format(test.name + " ") + Colors.FAIL + "FAIL" + Colors.ENDC)
+
+            if not test.success or verbose:
+                log = "        ".join(test.log.splitlines(True))
+                log = "        " + log
+                if log.strip():
+                    print(log)
+
+            passed_count += test.success
 
     end = time.time()
     elapsed = end - start
-    print()
     print('-' * 60)
-    passed_string = "    Passed {}/{}".format(passed_count, test_count)
-    print("{:<50} {:.2f}s".format(passed_string, elapsed))
+    passed_string = "Passed {}/{}".format(passed_count, tests_count)
+    print("{:<54} {:.2f}s".format(passed_string, elapsed))
 
-    return test_count - passed_count
+    return tests_count - passed_count
 
 
 def get_tests(path):
-    files = os.listdir(path)
-    files.sort()
+    packs = os.listdir(path)
     this_file = os.path.basename(__file__)
-    files = [elem for elem in files if elem != this_file]
-    return files
+    packs = [elem for elem in packs if elem != this_file]
+    packs.sort()
+
+    test_list = {}
+    for pack in packs:
+        pack_dir = os.path.join(path, pack)
+        test_files = os.listdir(pack_dir)
+        test_files.sort()
+
+        test_list[pack] = []
+
+        for test_name in test_files:
+            test_name = test_name.strip()
+            executable = os.path.join(test_dir, pack_dir, test_name)
+
+            if os.path.isdir(executable):
+                executable = os.path.join(executable, folder_test_script)
+
+            test_list[pack].append(Test(test_name, executable))
+
+    return test_list
 
 
 def show_help():
