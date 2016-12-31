eddf7c37a6355e7a38e3b11105b30454854be37c
alexiynew <lehafeda@gmail.com>
2016-12-31 21:42:04 +0300

    Update build script.

--- build.sh
+++ build.sh
@@ -1,14 +1,45 @@
 #!/bin/bash
 
-if [ -z "$CC" ]; then
-    echo "Specify C compiller as clang."
-    export CC=clang
-fi
+set -e
 
-if [ -z "$CXX" ]; then
-    echo "Specify C++ compiller as clang++."
-    export CXX=clang++
-fi
+CC_COMPILER=gcc
+CXX_COMILLER=g++
+
+function set_compiler {
+    if [ "$1" == "gcc" ]
+    then
+        CC_COMPILER=gcc
+        CXX_COMILLER=g++
+    elif [ "$1" == "clang" ]
+    then
+        CC_COMPILER=clang
+        CXX_COMILLER=clang++
+    else
+        echo "Unknown compiler: $1" >&2
+    fi
+
+    echo "Specify C compiller as $CC_COMPILER." >&2
+    export CC=$CC_COMPILER
+
+    echo "Specify C++ compiller as $CXX_COMILLER." >&2
+    export CXX=$CXX_COMILLER
+}
+
+while getopts ":c:" opt; do
+    case $opt in
+        c)
+            set_compiler $OPTARG >&2
+        ;;
+        \?)
+            echo "Invalid option -$OPTARG." >&2
+            exit 1
+        ;;
+        :)
+            echo "Option -$OPTARG requires an argument." >&2
+            exit 1
+        ;;
+    esac
+done
 
 echo "Start build in "$(pwd)
 mkdir -p build && cd ./build
@@ -17,3 +48,4 @@ cmake -DCMAKE_BUILD_TYPE=Release ../
 
 make -j4 all
 make CreateTestSuiteExecutable
+
