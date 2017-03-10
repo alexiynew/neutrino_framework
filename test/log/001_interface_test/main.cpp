b0f9fd7471d582604accf06f9282b555658aa763
alexiynew <lehafeda@gmail.com>
2017-03-10 15:21:05 +0300

    Add logger interface test.

--- test/log/001_interface_test/main.cpp
+++ test/log/001_interface_test/main.cpp
@@ -1,36 +1,86 @@
 #include <log/log.hpp>
+#include <string>
 #include <unit_test/suite.hpp>
+#include <vector>
 
-#pragma mark - tests for logger
+#pragma mark - helper functions
 
-class basic_logger_test : public framework::unit_test::suite
+std::string to_string(const framework::logger::logger_implementation::level level)
+{
+    using framework::logger::logger_implementation;
+    switch (level) {
+        case logger_implementation::level::debug: return "debug";
+        case logger_implementation::level::info: return "info";
+        case logger_implementation::level::warning: return "warning";
+        case logger_implementation::level::error: return "error";
+        default: return "UNKNOWN";
+    }
+}
+
+#pragma mark - simple logger to test interface
+
+class simple_logger : public framework::logger::logger_implementation
 {
 public:
-    basic_logger_test()
-        : suite("basic_logger_test")
+    void add_message(const logger_implementation::level message_level, const std::string& tag, const std::string& message) override
     {
-        add_test([this]() { first_test(); }, "first_test");
+        m_messages.push_back(to_string(message_level) + "_" + tag + "_" + message);
+    }
+
+    bool are_messages_equal_to(const std::vector<std::string>& example) const
+    {
+        return m_messages == example;
     }
 
 private:
-    void first_test()
+    std::vector<std::string> m_messages;
+};
+
+#pragma mark - tests for logger
+
+class logger_interface_test : public framework::unit_test::suite
+{
+public:
+    logger_interface_test()
+        : suite(suite_name)
+    {
+        add_test([this]() { simple_logger_test(); }, "simple_logger_test");
+    }
+
+private:
+    void simple_logger_test()
     {
         using framework::logger::log;
 
-        log::set_logger(std::make_unique<framework::logger::logger_implementation>());
+        log::set_logger(std::make_unique<simple_logger>());
 
-        log::debug("basic_logger_test", "debug message");
-        log::info("basic_logger_test", "info message");
-        log::warning("basic_logger_test", "warning message");
-        log::error("basic_logger_test", "error message");
+        log::debug(suite_name, "message_1");
+        log::info(suite_name, "message_2");
+        log::warning(suite_name, "message_3");
+        log::error(suite_name, "message_4");
 
-        TEST_FAIL("test");
+        using level = framework::logger::logger_implementation::level;
+
+        const std::vector<std::string> example{
+        to_string(level::debug) + "_" + suite_name + "_message_1",
+        to_string(level::info) + "_" + suite_name + "_message_2",
+        to_string(level::warning) + "_" + suite_name + "_message_3",
+        to_string(level::error) + "_" + suite_name + "_message_4",
+        };
+
+        const simple_logger* logger = static_cast<simple_logger*>(log::get_logger());
+
+        TEST_ASSERT(logger->are_messages_equal_to(example), "Log messages are not correct.");
     }
+
+    static const char* suite_name;
 };
+
+const char* logger_interface_test::suite_name = "logger_interface_test";
 
 #pragma mark - main function
 
 int main()
 {
-    return run_tests(basic_logger_test());
+    return run_tests(logger_interface_test());
 }
