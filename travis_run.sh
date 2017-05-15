5865f37f5d7abb10d02efcec2e05c418981f33fc
alexiynew <lehafeda@gmail.com>
2017-05-16 00:01:05 +0300

    Pass compiler variable to docker.

--- travis_run.sh
+++ travis_run.sh
@@ -7,13 +7,9 @@ set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR
 
-echo -e "Setup virtual display"
+# Setup virtual display
 Xvfb :1 -screen 0 1024x768x16 &> xvfb.log &
-echo -e "Setup virtual display 1"
-ps aux | grep X
-echo -e "Setup virtual display 2"
 export DISPLAY=:1.0
-echo -e "Setup virtual display 3"
 
-echo -e "Run build and tests"
+# Run build and tests
 ./build.sh -c $COMPILLER -t clean,build,test
