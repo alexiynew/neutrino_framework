bf0b96ec8d3d9c5ce7577d2880af2dd84bee71c0
alexiynew <lehafeda@gmail.com>
2017-06-20 21:53:08 +0300

    Fix test coverage setup.

--- build.sh
+++ build.sh
@@ -9,9 +9,9 @@ TASK_TO_RUN="none"
 
 TEST_MODULES=""
 
-RED='\033[0;31m' # Red
+RED='\033[0;31m'   # Red
 BLUE='\033[0;34m'  # Blue
-NC='\033[0m' # No Color
+NC='\033[0m'       # No Color
 
 function info {
     echo -e "${BLUE}$1${NC}";
@@ -44,6 +44,21 @@ function set_compiler {
 
     info "Specify C++ compiller as $CXX_COMILLER."
     export CXX="$CXX_COMILLER"
+}
+
+function check_x11_support {
+    if [[ "$(uname -s)" == "Linux" ]]
+    then
+        if [[ -n ${DISPLAY+x} ]]
+        then
+            info "==== Found existing display ===="
+        else
+            info "==== Setup virtual display ===="
+
+            Xvfb :1 -screen 0 1024x768x16 &> xvfb.log &
+            export DISPLAY=:1.0
+        fi
+    fi
 }
 
 # Task functions
@@ -79,12 +94,16 @@ function install_all {
 function run_tests {
     info "==== Run framework tests ===="
 
+    check_x11_support
+
     cd "$BUILD_DIR"
     make run_all_tests
 }
 
 function run_tests_verbose {
     info "==== Run framework tests verbose ===="
+
+    check_x11_support
 
     cd "$BUILD_DIR"
     make run_all_tests_verbose
@@ -95,21 +114,19 @@ function coverage_scan {
 
     mkdir -p "$BUILD_DIR"
     cd "$BUILD_DIR"
+
     cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTS=ON -DENABLE_TEST_COVERAGE=ON -DINCLUDED_TEST_MODULES=$TEST_MODULES ../
 
-    make -j4 all
+    build_framework
 
-    make -j4 framework_tests
+    build_tests
 
-    make run_all_tests
+    run_tests
 
+    info "==== Get lcov report ===="
     lcov --directory . --capture --output-file coverage.info                # capture coverage info
     lcov --remove coverage.info '/usr/*' --output-file coverage.info        # filter out system
     lcov --list coverage.info                                               # debug info
-
-    info "==== Uploading report to CodeCov ===="
-    #curl --tlsv1 -kv https://codecov.io/bash -o "upload.sh"
-    bash <(curl -s https://codecov.io/bash) || info "Codecov did not collect coverage reports"
 }
 
 function build_documentation {
@@ -127,30 +144,31 @@ function clean_all {
 }
 
 function print_help {
-    echo -e ""
-    echo -e "=== Help ==="
-    echo -e "./build.sh [OPTION VALUE[,VALUE]]"
-    echo -e ""
-    echo -e "OPTIONS:"
-    echo -e "\t -t : Specify task to run."
-    echo -e "\t VALUES:"
-    echo -e "\t\t configure    : Just runs cmake configuration."
-    echo -e "\t\t build        : Build framework and tests."
-    echo -e "\t\t install      : Install framework and tests."
-    echo -e "\t\t test         : Run all tests."
-    echo -e "\t\t test_verbose : Run all tests with verbose logging."
-    echo -e "\t\t coverage     : Run test coverage scan."
-    echo -e "\t\t docs         : Build documentation."
-    echo -e "\t\t clean        : Clean build results."
-    echo -e ""
-    echo -e "\t -c : Specify compiller to use."
-    echo -e "\t VALUES:"
-    echo -e "\t\t gcc   : Use gcc compiller (default)."
-    echo -e "\t\t clang : Use clang compiller."
-    echo -e ""
-    echo -e "\t -m : Specify which module you want to test."
-    echo -e "\t\t Parameters: <module>[,<module>]"
-    echo -e "\t\t If not present, all modules will be tested."
+cat << EOF
+==== Help ====
+    ./build.sh [OPTION VALUE[,VALUE]]
+
+    OPTIONS:
+        -t : Specify task to run.
+        VALUES:
+            configure    : Just runs cmake configuration.
+            build        : Build framework and tests.
+            install      : Install framework and tests.
+            test         : Run all tests.
+            test_verbose : Run all tests with verbose logging.
+            coverage     : Run test coverage scan.
+            docs         : Build documentation.
+            clean        : Clean build results.
+
+        -c : Specify compiller to use.
+        VALUES:
+            gcc   : Use gcc compiller (default).
+            clang : Use clang compiller.
+
+        -m : Specify which module you want to test.
+        Parameters: <module>[,<module>]
+            If not present, all modules will be tested.
+EOF
 }
 
 # Main logic
@@ -189,7 +207,7 @@ function run_task {
             print_help
             exit 1
         ;;
-        *) 
+        *)
             error "Unknown task '$1'"
             exit 1
         ;;
@@ -198,12 +216,14 @@ function run_task {
 
 function execute {
     IFS=', ' read -r -a array <<< "$1"
-    for task in "${array[@]}"; do
+    for task in "${array[@]}"
+    do
         run_task "$task"
     done
 }
 
-while getopts "t:c:m:h" opt; do
+while getopts "t:c:m:h" opt
+do
     case $opt in
         t)
             TASK_TO_RUN="$OPTARG"
