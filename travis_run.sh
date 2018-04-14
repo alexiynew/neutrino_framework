ff94057230c0a880be9e4e7cd93c7ce3738d961b
alexiynew <lehafeda@gmail.com>
2018-04-14 12:44:07 +0300

    Fix build.

--- travis_run.sh
+++ travis_run.sh
@@ -7,8 +7,8 @@ set -e
 # Run build and tests
 if [[ "${TRAVIS_OS_NAME}" == "linux" ]]
 then
-    local command="xvfb-run -a -s \"-screen 0 1024x768x24\" /home/framework/build.sh -c ${COMPILLER} ${TASK}"
-    sudo docker run -it -v "${TRAVIS_BUILD_DIR}:/home/framework" alexiynew/docker_image bash -c "${command}"
+    COMMAND="xvfb-run -a -s \"-screen 0 1024x768x24\" /home/framework/build.sh -c ${COMPILLER} ${TASK}"
+    sudo docker run -it -v "${TRAVIS_BUILD_DIR}:/home/framework" alexiynew/docker_image bash -c "${COMMAND}"
 
     if [[ "${TASK}" == *"coverage"* ]]
     then
