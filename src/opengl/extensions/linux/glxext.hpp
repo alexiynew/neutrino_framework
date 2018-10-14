ddd88db5eab421cd9c58385b4f9749dc69c2cb9a
alexiynew <lehafeda@gmail.com>
2018-10-14 12:15:44 +0300

    [#19] Add GLX wrapper generation.

---
+++ src/opengl/extensions/linux/glxext.hpp
@@ -0,0 +1,373 @@
+/// @file
+/// @brief GLX extension functions wrapper.
+/// @author Fedorov Alexey
+/// @date 17.09.2018
+
+// =============================================================================
+// MIT License
+//
+// Copyright (c) 2017-2018 Fedorov Alexey
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// =============================================================================
+
+#ifndef FRAMEWORK_OPENGL_EXTENSIONS_LINUX_GLXEXT_HPP
+#define FRAMEWORK_OPENGL_EXTENSIONS_LINUX_GLXEXT_HPP
+
+#include <GL/glcorearb.h>
+#include <GL/glxext.h>
+
+namespace framework::opengl
+{
+extern bool glx_version_1_3_supported;
+extern bool glx_version_1_4_supported;
+extern bool glx_arb_create_context_supported;
+extern bool glx_arb_get_proc_address_supported;
+extern bool glx_amd_gpu_association_supported;
+extern bool glx_ext_import_context_supported;
+extern bool glx_ext_swap_control_supported;
+extern bool glx_ext_texture_from_pixmap_supported;
+extern bool glx_mesa_agp_offset_supported;
+extern bool glx_mesa_copy_sub_buffer_supported;
+extern bool glx_mesa_pixmap_colormap_supported;
+extern bool glx_mesa_query_renderer_supported;
+extern bool glx_mesa_release_buffers_supported;
+extern bool glx_mesa_set_3dfx_mode_supported;
+extern bool glx_mesa_swap_control_supported;
+extern bool glx_nv_copy_buffer_supported;
+extern bool glx_nv_copy_image_supported;
+extern bool glx_nv_delay_before_swap_supported;
+extern bool glx_nv_present_video_supported;
+extern bool glx_nv_swap_group_supported;
+extern bool glx_nv_video_capture_supported;
+extern bool glx_nv_video_out_supported;
+extern bool glx_oml_sync_control_supported;
+extern bool glx_sgix_dmbuffer_supported;
+extern bool glx_sgix_fbconfig_supported;
+extern bool glx_sgix_hyperpipe_supported;
+extern bool glx_sgix_pbuffer_supported;
+extern bool glx_sgix_swap_barrier_supported;
+extern bool glx_sgix_swap_group_supported;
+extern bool glx_sgix_video_resize_supported;
+extern bool glx_sgix_video_source_supported;
+extern bool glx_sgi_cushion_supported;
+extern bool glx_sgi_make_current_read_supported;
+extern bool glx_sgi_swap_control_supported;
+extern bool glx_sgi_video_sync_supported;
+extern bool glx_sun_get_transparent_index_supported;
+
+#pragma region GLX_VERSION_1_3
+
+extern PFNGLXGETFBCONFIGSPROC glXGetFBConfigs;
+extern PFNGLXCHOOSEFBCONFIGPROC glXChooseFBConfig;
+extern PFNGLXGETFBCONFIGATTRIBPROC glXGetFBConfigAttrib;
+extern PFNGLXGETVISUALFROMFBCONFIGPROC glXGetVisualFromFBConfig;
+extern PFNGLXCREATEWINDOWPROC glXCreateWindow;
+extern PFNGLXDESTROYWINDOWPROC glXDestroyWindow;
+extern PFNGLXCREATEPIXMAPPROC glXCreatePixmap;
+extern PFNGLXDESTROYPIXMAPPROC glXDestroyPixmap;
+extern PFNGLXCREATEPBUFFERPROC glXCreatePbuffer;
+extern PFNGLXDESTROYPBUFFERPROC glXDestroyPbuffer;
+extern PFNGLXQUERYDRAWABLEPROC glXQueryDrawable;
+extern PFNGLXCREATENEWCONTEXTPROC glXCreateNewContext;
+extern PFNGLXMAKECONTEXTCURRENTPROC glXMakeContextCurrent;
+extern PFNGLXGETCURRENTREADDRAWABLEPROC glXGetCurrentReadDrawable;
+extern PFNGLXQUERYCONTEXTPROC glXQueryContext;
+extern PFNGLXSELECTEVENTPROC glXSelectEvent;
+extern PFNGLXGETSELECTEDEVENTPROC glXGetSelectedEvent;
+
+#pragma endregion
+
+#pragma region GLX_VERSION_1_4
+
+extern PFNGLXGETPROCADDRESSPROC glXGetProcAddress;
+
+#pragma endregion
+
+#pragma region GLX_ARB_create_context
+
+extern PFNGLXCREATECONTEXTATTRIBSARBPROC glXCreateContextAttribsARB;
+
+#pragma endregion
+
+#pragma region GLX_ARB_get_proc_address
+
+extern PFNGLXGETPROCADDRESSARBPROC glXGetProcAddressARB;
+
+#pragma endregion
+
+#pragma region GLX_AMD_gpu_association
+
+extern PFNGLXGETGPUIDSAMDPROC glXGetGPUIDsAMD;
+extern PFNGLXGETGPUINFOAMDPROC glXGetGPUInfoAMD;
+extern PFNGLXGETCONTEXTGPUIDAMDPROC glXGetContextGPUIDAMD;
+extern PFNGLXCREATEASSOCIATEDCONTEXTAMDPROC glXCreateAssociatedContextAMD;
+extern PFNGLXCREATEASSOCIATEDCONTEXTATTRIBSAMDPROC glXCreateAssociatedContextAttribsAMD;
+extern PFNGLXDELETEASSOCIATEDCONTEXTAMDPROC glXDeleteAssociatedContextAMD;
+extern PFNGLXMAKEASSOCIATEDCONTEXTCURRENTAMDPROC glXMakeAssociatedContextCurrentAMD;
+extern PFNGLXGETCURRENTASSOCIATEDCONTEXTAMDPROC glXGetCurrentAssociatedContextAMD;
+extern PFNGLXBLITCONTEXTFRAMEBUFFERAMDPROC glXBlitContextFramebufferAMD;
+
+#pragma endregion
+
+#pragma region GLX_EXT_import_context
+
+extern PFNGLXGETCURRENTDISPLAYEXTPROC glXGetCurrentDisplayEXT;
+extern PFNGLXQUERYCONTEXTINFOEXTPROC glXQueryContextInfoEXT;
+extern PFNGLXGETCONTEXTIDEXTPROC glXGetContextIDEXT;
+extern PFNGLXIMPORTCONTEXTEXTPROC glXImportContextEXT;
+extern PFNGLXFREECONTEXTEXTPROC glXFreeContextEXT;
+
+#pragma endregion
+
+#pragma region GLX_EXT_swap_control
+
+extern PFNGLXSWAPINTERVALEXTPROC glXSwapIntervalEXT;
+
+#pragma endregion
+
+#pragma region GLX_EXT_texture_from_pixmap
+
+extern PFNGLXBINDTEXIMAGEEXTPROC glXBindTexImageEXT;
+extern PFNGLXRELEASETEXIMAGEEXTPROC glXReleaseTexImageEXT;
+
+#pragma endregion
+
+#pragma region GLX_MESA_agp_offset
+
+extern PFNGLXGETAGPOFFSETMESAPROC glXGetAGPOffsetMESA;
+
+#pragma endregion
+
+#pragma region GLX_MESA_copy_sub_buffer
+
+extern PFNGLXCOPYSUBBUFFERMESAPROC glXCopySubBufferMESA;
+
+#pragma endregion
+
+#pragma region GLX_MESA_pixmap_colormap
+
+extern PFNGLXCREATEGLXPIXMAPMESAPROC glXCreateGLXPixmapMESA;
+
+#pragma endregion
+
+#pragma region GLX_MESA_query_renderer
+
+extern PFNGLXQUERYCURRENTRENDERERINTEGERMESAPROC glXQueryCurrentRendererIntegerMESA;
+extern PFNGLXQUERYCURRENTRENDERERSTRINGMESAPROC glXQueryCurrentRendererStringMESA;
+extern PFNGLXQUERYRENDERERINTEGERMESAPROC glXQueryRendererIntegerMESA;
+extern PFNGLXQUERYRENDERERSTRINGMESAPROC glXQueryRendererStringMESA;
+
+#pragma endregion
+
+#pragma region GLX_MESA_release_buffers
+
+extern PFNGLXRELEASEBUFFERSMESAPROC glXReleaseBuffersMESA;
+
+#pragma endregion
+
+#pragma region GLX_MESA_set_3dfx_mode
+
+extern PFNGLXSET3DFXMODEMESAPROC glXSet3DfxModeMESA;
+
+#pragma endregion
+
+#pragma region GLX_MESA_swap_control
+
+extern PFNGLXGETSWAPINTERVALMESAPROC glXGetSwapIntervalMESA;
+extern PFNGLXSWAPINTERVALMESAPROC glXSwapIntervalMESA;
+
+#pragma endregion
+
+#pragma region GLX_NV_copy_buffer
+
+extern PFNGLXCOPYBUFFERSUBDATANVPROC glXCopyBufferSubDataNV;
+extern PFNGLXNAMEDCOPYBUFFERSUBDATANVPROC glXNamedCopyBufferSubDataNV;
+
+#pragma endregion
+
+#pragma region GLX_NV_copy_image
+
+extern PFNGLXCOPYIMAGESUBDATANVPROC glXCopyImageSubDataNV;
+
+#pragma endregion
+
+#pragma region GLX_NV_delay_before_swap
+
+extern PFNGLXDELAYBEFORESWAPNVPROC glXDelayBeforeSwapNV;
+
+#pragma endregion
+
+#pragma region GLX_NV_present_video
+
+extern PFNGLXENUMERATEVIDEODEVICESNVPROC glXEnumerateVideoDevicesNV;
+extern PFNGLXBINDVIDEODEVICENVPROC glXBindVideoDeviceNV;
+
+#pragma endregion
+
+#pragma region GLX_NV_swap_group
+
+extern PFNGLXJOINSWAPGROUPNVPROC glXJoinSwapGroupNV;
+extern PFNGLXBINDSWAPBARRIERNVPROC glXBindSwapBarrierNV;
+extern PFNGLXQUERYSWAPGROUPNVPROC glXQuerySwapGroupNV;
+extern PFNGLXQUERYMAXSWAPGROUPSNVPROC glXQueryMaxSwapGroupsNV;
+extern PFNGLXQUERYFRAMECOUNTNVPROC glXQueryFrameCountNV;
+extern PFNGLXRESETFRAMECOUNTNVPROC glXResetFrameCountNV;
+
+#pragma endregion
+
+#pragma region GLX_NV_video_capture
+
+extern PFNGLXBINDVIDEOCAPTUREDEVICENVPROC glXBindVideoCaptureDeviceNV;
+extern PFNGLXENUMERATEVIDEOCAPTUREDEVICESNVPROC glXEnumerateVideoCaptureDevicesNV;
+extern PFNGLXLOCKVIDEOCAPTUREDEVICENVPROC glXLockVideoCaptureDeviceNV;
+extern PFNGLXQUERYVIDEOCAPTUREDEVICENVPROC glXQueryVideoCaptureDeviceNV;
+extern PFNGLXRELEASEVIDEOCAPTUREDEVICENVPROC glXReleaseVideoCaptureDeviceNV;
+
+#pragma endregion
+
+#pragma region GLX_NV_video_out
+
+extern PFNGLXGETVIDEODEVICENVPROC glXGetVideoDeviceNV;
+extern PFNGLXRELEASEVIDEODEVICENVPROC glXReleaseVideoDeviceNV;
+extern PFNGLXBINDVIDEOIMAGENVPROC glXBindVideoImageNV;
+extern PFNGLXRELEASEVIDEOIMAGENVPROC glXReleaseVideoImageNV;
+extern PFNGLXSENDPBUFFERTOVIDEONVPROC glXSendPbufferToVideoNV;
+extern PFNGLXGETVIDEOINFONVPROC glXGetVideoInfoNV;
+
+#pragma endregion
+
+#pragma region GLX_OML_sync_control
+
+extern PFNGLXGETSYNCVALUESOMLPROC glXGetSyncValuesOML;
+extern PFNGLXGETMSCRATEOMLPROC glXGetMscRateOML;
+extern PFNGLXSWAPBUFFERSMSCOMLPROC glXSwapBuffersMscOML;
+extern PFNGLXWAITFORMSCOMLPROC glXWaitForMscOML;
+extern PFNGLXWAITFORSBCOMLPROC glXWaitForSbcOML;
+
+#pragma endregion
+
+#pragma region GLX_SGIX_dmbuffer
+
+extern PFNGLXASSOCIATEDMPBUFFERSGIXPROC glXAssociateDMPbufferSGIX;
+
+#pragma endregion
+
+#pragma region GLX_SGIX_fbconfig
+
+extern PFNGLXGETFBCONFIGATTRIBSGIXPROC glXGetFBConfigAttribSGIX;
+extern PFNGLXCHOOSEFBCONFIGSGIXPROC glXChooseFBConfigSGIX;
+extern PFNGLXCREATEGLXPIXMAPWITHCONFIGSGIXPROC glXCreateGLXPixmapWithConfigSGIX;
+extern PFNGLXCREATECONTEXTWITHCONFIGSGIXPROC glXCreateContextWithConfigSGIX;
+extern PFNGLXGETVISUALFROMFBCONFIGSGIXPROC glXGetVisualFromFBConfigSGIX;
+extern PFNGLXGETFBCONFIGFROMVISUALSGIXPROC glXGetFBConfigFromVisualSGIX;
+
+#pragma endregion
+
+#pragma region GLX_SGIX_hyperpipe
+
+extern PFNGLXQUERYHYPERPIPENETWORKSGIXPROC glXQueryHyperpipeNetworkSGIX;
+extern PFNGLXHYPERPIPECONFIGSGIXPROC glXHyperpipeConfigSGIX;
+extern PFNGLXQUERYHYPERPIPECONFIGSGIXPROC glXQueryHyperpipeConfigSGIX;
+extern PFNGLXDESTROYHYPERPIPECONFIGSGIXPROC glXDestroyHyperpipeConfigSGIX;
+extern PFNGLXBINDHYPERPIPESGIXPROC glXBindHyperpipeSGIX;
+extern PFNGLXQUERYHYPERPIPEBESTATTRIBSGIXPROC glXQueryHyperpipeBestAttribSGIX;
+extern PFNGLXHYPERPIPEATTRIBSGIXPROC glXHyperpipeAttribSGIX;
+extern PFNGLXQUERYHYPERPIPEATTRIBSGIXPROC glXQueryHyperpipeAttribSGIX;
+
+#pragma endregion
+
+#pragma region GLX_SGIX_pbuffer
+
+extern PFNGLXCREATEGLXPBUFFERSGIXPROC glXCreateGLXPbufferSGIX;
+extern PFNGLXDESTROYGLXPBUFFERSGIXPROC glXDestroyGLXPbufferSGIX;
+extern PFNGLXQUERYGLXPBUFFERSGIXPROC glXQueryGLXPbufferSGIX;
+extern PFNGLXSELECTEVENTSGIXPROC glXSelectEventSGIX;
+extern PFNGLXGETSELECTEDEVENTSGIXPROC glXGetSelectedEventSGIX;
+
+#pragma endregion
+
+#pragma region GLX_SGIX_swap_barrier
+
+extern PFNGLXBINDSWAPBARRIERSGIXPROC glXBindSwapBarrierSGIX;
+extern PFNGLXQUERYMAXSWAPBARRIERSSGIXPROC glXQueryMaxSwapBarriersSGIX;
+
+#pragma endregion
+
+#pragma region GLX_SGIX_swap_group
+
+extern PFNGLXJOINSWAPGROUPSGIXPROC glXJoinSwapGroupSGIX;
+
+#pragma endregion
+
+#pragma region GLX_SGIX_video_resize
+
+extern PFNGLXBINDCHANNELTOWINDOWSGIXPROC glXBindChannelToWindowSGIX;
+extern PFNGLXCHANNELRECTSGIXPROC glXChannelRectSGIX;
+extern PFNGLXQUERYCHANNELRECTSGIXPROC glXQueryChannelRectSGIX;
+extern PFNGLXQUERYCHANNELDELTASSGIXPROC glXQueryChannelDeltasSGIX;
+extern PFNGLXCHANNELRECTSYNCSGIXPROC glXChannelRectSyncSGIX;
+
+#pragma endregion
+
+#pragma region GLX_SGIX_video_source
+
+extern PFNGLXCREATEGLXVIDEOSOURCESGIXPROC glXCreateGLXVideoSourceSGIX;
+extern PFNGLXDESTROYGLXVIDEOSOURCESGIXPROC glXDestroyGLXVideoSourceSGIX;
+
+#pragma endregion
+
+#pragma region GLX_SGI_cushion
+
+extern PFNGLXCUSHIONSGIPROC glXCushionSGI;
+
+#pragma endregion
+
+#pragma region GLX_SGI_make_current_read
+
+extern PFNGLXMAKECURRENTREADSGIPROC glXMakeCurrentReadSGI;
+extern PFNGLXGETCURRENTREADDRAWABLESGIPROC glXGetCurrentReadDrawableSGI;
+
+#pragma endregion
+
+#pragma region GLX_SGI_swap_control
+
+extern PFNGLXSWAPINTERVALSGIPROC glXSwapIntervalSGI;
+
+#pragma endregion
+
+#pragma region GLX_SGI_video_sync
+
+extern PFNGLXGETVIDEOSYNCSGIPROC glXGetVideoSyncSGI;
+extern PFNGLXWAITVIDEOSYNCSGIPROC glXWaitVideoSyncSGI;
+
+#pragma endregion
+
+#pragma region GLX_SUN_get_transparent_index
+
+extern PFNGLXGETTRANSPARENTINDEXSUNPROC glXGetTransparentIndexSUN;
+
+#pragma endregion
+
+/// Initialize GLX functions
+void init_glx();
+
+} // namespace framework::opengl
+
+#endif
---
+++ tools/generate_glx.py
@@ -0,0 +1,261 @@
+#! /usr/bin/python3
+
+import re
+from string import Template
+
+source = "./dependencies/GL/glxext.h"
+desthpp = "./src/opengl/extensions/linux/glxext.hpp"
+destcpp = "./src/opengl/extensions/linux/glxext.cpp"
+
+group_regex = r'#ifndef\s([\w\d_]+)\s#define\s\1\s1\s(.*?)#endif\s/\*\s\1\s\*/'
+type_regex = r'typedef.*\(\s?\*(PFN.*PROC)\).*;'
+name_regex = r'.*(glX[\w\d_]+)\s?\(.*;'
+
+license = "// =============================================================================\n" \
+          "// MIT License\n" \
+          "//\n" \
+          "// Copyright (c) 2017-2018 Fedorov Alexey\n" \
+          "//\n" \
+          "// Permission is hereby granted, free of charge, to any person obtaining a copy\n" \
+          "// of this software and associated documentation files (the \"Software\"), to deal\n" \
+          "// in the Software without restriction, including without limitation the rights\n" \
+          "// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n" \
+          "// copies of the Software, and to permit persons to whom the Software is\n" \
+          "// furnished to do so, subject to the following conditions:\n" \
+          "//\n" \
+          "// The above copyright notice and this permission notice shall be included in all\n" \
+          "// copies or substantial portions of the Software.\n" \
+          "//\n" \
+          "// THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n" \
+          "// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n" \
+          "// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n" \
+          "// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n" \
+          "// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n" \
+          "// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n" \
+          "// SOFTWARE.\n" \
+          "// =============================================================================\n"
+
+
+class func:
+    type = ''
+    name = ''
+
+    def __init__(self, t, n):
+        self.type = t
+        self.name = n
+
+
+class section:
+    name = ''
+    lower_name = ''
+    functions = []
+
+    def __init__(self, name, functions):
+        self.name = name
+        self.lower_name = name.lower()
+        self.functions = functions
+
+
+def get_sections():
+    sections = []
+
+    file = open(source, "r")
+
+    groups = re.findall(group_regex, file.read(), re.S)
+
+    for g in groups:
+        types = re.findall(type_regex, g[1])
+        names = re.findall(name_regex, g[1])
+
+        functions = (
+            list(map(lambda f: func(f[0], f[1]), list(zip(types, names)))))
+
+        sections.append(section(g[0], functions))
+
+    file.close()
+
+    return sections
+
+
+def generate_declarations(sections):
+    t = Template("\n"
+                 "#pragma region ${name}\n"
+                 "\n"
+                 "${functions}\n"
+                 "\n"
+                 "#pragma endregion\n")
+
+    result = ""
+    for s in sections:
+        d = dict(name=s.name,
+                 functions="\n".join("extern {f.type} {f.name};".format(f=f) for f in s.functions))
+
+        result += t.substitute(d)
+
+    return result
+
+
+def generate_header(sections):
+    t = "/// @file\n" \
+        "/// @brief GLX extension functions wrapper.\n" \
+        "/// @author Fedorov Alexey\n" \
+        "/// @date 17.09.2018\n" \
+        "\n" \
+        "{license}" \
+        "\n" \
+        "#ifndef FRAMEWORK_OPENGL_EXTENSIONS_LINUX_GLXEXT_HPP\n" \
+        "#define FRAMEWORK_OPENGL_EXTENSIONS_LINUX_GLXEXT_HPP\n" \
+        "\n" \
+        "#include <GL/glcorearb.h>\n" \
+        "#include <GL/glxext.h>\n" \
+        "\n" \
+        "namespace framework::opengl\n" \
+        "{{\n" \
+        "{is_glx_extension_supported}" \
+        "\n" \
+        "{declarations}" \
+        "\n" \
+        "/// Initialize GLX functions\n" \
+        "void init_glx();\n" \
+        "\n" \
+        "}} // namespace framework::opengl\n" \
+        "\n" \
+        "#endif\n"
+
+    is_glx_extension_supported = "\n".join(
+        "extern bool {s.lower_name}_supported;".format(s=s) for s in sections)
+
+    hpp = open(desthpp, "w")
+    hpp.write(t.format(license=license,
+                       is_glx_extension_supported=is_glx_extension_supported,
+                       declarations=generate_declarations(sections)))
+    hpp.close()
+
+
+def generate_init_declarations(sections):
+    t = Template("#pragma region init_declarations\n"
+                 "\n"
+                 "${init_functions}"
+                 "\n"
+                 "void init_extensions();\n"
+                 "\n"
+                 "#pragma endregion\n")
+
+    return t.substitute(init_functions="\n".join("bool init_{0}();".format(s.name.lower()) for s in sections) + "\n")
+
+
+def generate_null_definitions(sections):
+    t = Template("#pragma region ${name}\n"
+                 "\n"
+                 "${null_definition}"
+                 "\n"
+                 "#pragma endregion\n")
+
+    result = ""
+
+    return "\n".join(t.substitute(name=s.name,
+                                  null_definition="\n".join("{0} {1} = nullptr;".format(
+                                      f.type, f.name) for f in s.functions) + "\n") for s in sections)
+
+
+def generate_init_definitions(sections):
+    t = Template("bool init_${lower_name}()\n"
+                 "{\n"
+                 "    bool result = true;\n"
+                 "\n"
+                 "    // clang-format off\n"
+                 "${assignment}"
+                 "\n"
+                 "    // clang-format on\n"
+                 "\n"
+                 "    return result;\n"
+                 "}\n")
+
+    impl_str = "    result = result && (framework::opengl::{0} = get_function<{1}>(\"{0}\")) != nullptr;"
+
+    init_functions = "\n".join(t.substitute(name=s.name,
+                                            lower_name=s.name.lower(),
+                                            assignment="\n".join(impl_str.format(f.name, f.type) for f in s.functions)) for s in sections)
+
+    e = Template("#pragma region init_definitions\n"
+                 "\n"
+                 "namespace\n"
+                 "{\n"
+                 "${init_functions}"
+                 "\n"
+                 "void init_extensions()\n"
+                 "{\n"
+                 "    // clang-format off\n"
+                 "${assignment_glx_extensions}"
+                 "    // clang-format on\n"
+                 "\n"
+                 "}\n"
+                 "\n"
+                 "} // namespace\n"
+                 "\n"
+                 "#pragma endregion\n")
+
+    return e.substitute(init_functions=init_functions,
+                        assignment_glx_extensions="\n".join("    framework::opengl::{0}_supported = init_{0}();".format(s.lower_name) for s in sections) + "\n")
+
+
+def generate_source(sections):
+    t = Template("/// @file\n"
+                 "/// @brief GLX extension functions wrapper.\n"
+                 "/// @author Fedorov Alexey\n"
+                 "/// @date 16.09.2018\n"
+                 "\n"
+                 "${license}"
+                 "\n"
+                 "#include <array>\n"
+                 "#include <mutex>\n"
+                 "\n"
+                 "#include <common/types.hpp>\n"
+                 "#include <opengl/details/gl_details.hpp>\n"
+                 "#include <opengl/extensions/linux/glxext.hpp>\n"
+                 "\n"
+                 "namespace\n"
+                 "{\n"
+                 "std::once_flag init_flag;\n"
+                 "\n"
+                 "template <typename F>\n"
+                 "F get_function(const char* function_name)\n"
+                 "{\n"
+                 "    return reinterpret_cast<F>(framework::opengl::details::get_function(function_name));\n"
+                 "}\n"
+                 "\n"
+                 "${init_declarations}"
+                 "\n"
+                 "} // namespace\n"
+                 "\n"
+                 "namespace framework::opengl\n"
+                 "{\n"
+                 "${is_glx_extension_supported}"
+                 "\n"
+                 "${null_definitions}"
+                 "\n"
+                 "void init_glx()\n"
+                 "{\n"
+                 "    std::call_once(init_flag, []() { ::init_extensions(); });\n"
+                 "}\n"
+                 "\n"
+                 "} // namespace framework::opengl\n"
+                 "\n"
+                 "${init_definitions}")
+
+    d = dict(license=license,
+             is_glx_extension_supported="\n".join(
+                 "bool {s.lower_name}_supported = false;".format(s=s) for s in sections) + "\n",
+             init_declarations=generate_init_declarations(sections),
+             null_definitions=generate_null_definitions(sections),
+             init_definitions=generate_init_definitions(sections))
+
+    cpp = open(destcpp, "w")
+    cpp.write(t.substitute(d))
+    cpp.close()
+
+
+sections = list(filter(lambda s: len(s.functions) > 0, get_sections()))
+
+generate_header(sections)
+generate_source(sections)
