78ea4a965dc5e60b89430ad195dc47915da4b601
Федоров Алексей Анатольевич <16724363@sigma.sbrf.ru>
2019-04-29 16:45:04 +0300

    [#29] Fix ortho2d projection test.

--- test/math/matrix_transform/main.cpp
+++ test/math/matrix_transform/main.cpp
@@ -440,10 +440,10 @@ private:
     {
         // clang-format off
         const matrix4f proj2D = {
-            0.5f, 0,    0, 0,
-            0,    0.5f, 0, 0,
-            0,    0,    1, 0,
-            0,    0,    0, 1
+            0.5f, 0,    0,  0,
+            0,    0.5f, 0,  0,
+            0,    0,    -1, 0,
+            0,    0,    0,  1
         };
         // clang-format on
 
