9a85efe59235732941b78312f7f83950b3b476c6
alexiynew <lehafeda@gmail.com>
2020-04-09 21:34:45 +0300

    [#35] Add fps counter test

---
+++ test/common/fps_counter/main.cpp
@@ -0,0 +1,71 @@
+
+// =============================================================================
+// MIT License
+//
+// Copyright (c) 2017-2019 Fedorov Alexey
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+// =============================================================================
+
+#include <common/fps_counter.hpp>
+#include <unit_test/suite.hpp>
+
+#include <thread>
+#include <log/log.hpp>
+#include <chrono>
+
+using namespace framework;
+
+class fps_counter_test : public unit_test::Suite
+{
+public:
+    fps_counter_test()
+        : Suite("fps_counter_test")
+    {
+        add_test([this]() { fps_count(); }, "fps_count");
+    }
+
+private:
+    void fps_count()
+    {
+        FpsCounter fps;
+
+        for (int i = 0; i < 100; ++i)
+        {
+            fps.tick();
+        }
+
+        while (fps.fps() == 0) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(5));
+        }
+
+        TEST_ASSERT(fps.fps() == 100, "Should be 100 frames");
+
+        while (fps.fps() != 0) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(5));
+        }
+
+        TEST_ASSERT(fps.fps() == 0, "Should be 0 frames");
+    }
+};
+
+int main()
+{
+    return run_tests(fps_counter_test());
+}
