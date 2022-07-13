c77d8545d6a1ea3ed82a75a7c8ddb7b1b50a209f
alexiynew <lehafeda@gmail.com>
2022-07-13 13:07:12 +0300

    [#23] Cleanup polygon functions

--- neutrino/math/src/polygon_functions.cpp
+++ neutrino/math/src/polygon_functions.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <numeric>
 #include <stdexcept>
 
@@ -6,7 +7,90 @@
 #include <math/inc/polygon_functions.hpp>
 #undef FRAMEWORK_MATH_DETAILS
 
-#include <profiler/profiler.hpp>
+namespace
+{
+using namespace framework::math;
+
+struct PointInfo
+{
+    std::uint32_t prev_index = 0;
+    std::uint32_t curr_index = 0;
+    std::uint32_t next_index = 0;
+    float dot                = 0;
+    bool is_ear              = false;
+};
+
+void update_info(std::vector<PointInfo>& indices,
+                 std::size_t index_in_indices,
+                 const Polygon& polygon,
+                 const bool is_ccw)
+{
+    const auto prev_index_in_indices = (index_in_indices + indices.size() - 1) % indices.size();
+    const auto next_index_in_indices = (index_in_indices + 1) % indices.size();
+
+    const std::uint32_t prev_point_index = indices[prev_index_in_indices].curr_index;
+    const std::uint32_t curr_point_index = indices[index_in_indices].curr_index;
+    const std::uint32_t next_point_index = indices[next_index_in_indices].curr_index;
+
+    const auto& prev = polygon[prev_point_index];
+    const auto& curr = polygon[curr_point_index];
+    const auto& next = polygon[next_point_index];
+
+    // update point neighbors indices
+    indices[index_in_indices].next_index = next_point_index;
+    indices[index_in_indices].prev_index = prev_point_index;
+
+    // update dot
+    indices[index_in_indices].dot = dot(normalize(prev - curr), normalize(next - curr));
+
+    // update is ear
+    const auto c           = cross(prev - curr, next - curr);
+    const bool valid_angle = !((!is_ccw && c <= 0.0f) || (is_ccw && c >= 0.0f));
+
+    if (valid_angle) {
+        indices[index_in_indices].is_ear = [&]() {
+            for (const auto& pi : indices) {
+                const auto& p = polygon[pi.curr_index];
+                if (p == prev || p == curr || p == next) {
+                    continue;
+                }
+
+                if (is_point_in_triangle(p, prev, next, curr)) {
+                    return false;
+                }
+            }
+            return true;
+        }();
+    } else {
+        indices[index_in_indices].is_ear = false;
+    }
+};
+
+std::size_t next_ear_index(const std::vector<PointInfo>& indices)
+{
+    std::vector<std::size_t> ears;
+    ears.reserve(indices.size());
+
+    // find all ears
+    for (std::size_t i = 0; i < indices.size(); ++i) {
+        if (indices[i].is_ear) {
+            ears.push_back(i);
+        }
+    }
+
+    if (ears.empty()) {
+        throw std::runtime_error("Ears are empty. It's bug in triangulation algorithm.");
+    }
+
+    // get best ear
+    const auto it = std::max_element(ears.begin(), ears.end(), [&indices](std::size_t a, std::size_t b) {
+        return indices[a].dot < indices[b].dot;
+    });
+
+    return *it;
+}
+
+} // namespace
 
 namespace framework::math
 {
@@ -67,130 +151,47 @@ bool is_point_in_polygon(const Vector<2, float>& point, const Polygon& polygon)
     return intersections_num % 2 == 1;
 }
 
-struct PointInfo
-{
-    std::uint32_t prev_index = 0;
-    std::uint32_t curr_index = 0;
-    std::uint32_t next_index = 0;
-    bool is_ear              = false;
-    float dot                = 0;
-};
-
 std::vector<std::uint32_t> generate_ear_cut_triangulation(const Polygon& polygon)
 {
-    [[maybe_unused]] const auto& p1 = profiler::count_scope("generation");
-
     const bool is_ccw = polygon_area(polygon) < 0;
 
+    // fill initial indices
     std::vector<PointInfo> indices(polygon.size());
-    std::generate(indices.begin(), indices.end(), [n = 0]() mutable {
-        return PointInfo{0, static_cast<std::uint32_t>(n++), 0, true};
-    });
-
-    auto update_info = [&](std::size_t index_in_indices) {
-        const auto prev_index_in_indices = (index_in_indices + indices.size() - 1) % indices.size();
-        const auto next_index_in_indices = (index_in_indices + 1) % indices.size();
-
-        const std::uint32_t prev_point_index = indices[prev_index_in_indices].curr_index;
-        const std::uint32_t curr_point_index = indices[index_in_indices].curr_index;
-        const std::uint32_t next_point_index = indices[next_index_in_indices].curr_index;
-
-        const Vector<2, float>& prev = polygon[prev_point_index];
-        const Vector<2, float>& curr = polygon[curr_point_index];
-        const Vector<2, float>& next = polygon[next_point_index];
-
-        // update point neighbors indices
-        indices[index_in_indices].next_index = next_point_index;
-        indices[index_in_indices].prev_index = prev_point_index;
-
-        // update dot
-        indices[index_in_indices].dot = dot(normalize(prev - curr), normalize(next - curr));
-
-        // update is ear
-        const auto c           = cross(prev - curr, next - curr);
-        const bool valid_angle = !((!is_ccw && c <= 0.0f) || (is_ccw && c >= 0.0f));
-
-        if (valid_angle) {
-            indices[index_in_indices].is_ear = [&]() {
-                for (const auto& pi : indices) {
-                    const auto& p = polygon[pi.curr_index];
-                    if (p == prev || p == curr || p == next) {
-                        continue;
-                    }
-
-                    if (is_point_in_triangle(p, prev, next, curr)) {
-                        return false;
-                    }
-                }
-                return true;
-            }();
-        } else {
-            indices[index_in_indices].is_ear = false;
-        }
-    };
-
-    {
-        [[maybe_unused]] const auto& p2 = profiler::count_scope("precalc");
-
-        for (size_t i = 0; i < indices.size(); ++i) {
-            update_info(i);
-        }
+    for (std::uint32_t i = 0; i < indices.size(); ++i) {
+        indices[i].curr_index = i;
     }
 
+    // precalculate point infos
+    for (size_t i = 0; i < indices.size(); ++i) {
+        update_info(indices, i, polygon, is_ccw);
+    }
+
+    // triangles for triangulation result
     std::vector<std::uint32_t> triangles;
     triangles.reserve((polygon.size() - 2) * 3);
 
     while (indices.size() > 3) {
+        const auto ear_index   = next_ear_index(indices);
+        const auto& point_info = indices[ear_index];
 
-        [[maybe_unused]] const auto& p2 = profiler::count_scope("loop");
-        std::vector<std::size_t> ears;
-        ears.reserve(indices.size());
-
-        {
-            [[maybe_unused]] const auto& p3 = profiler::count_scope("ears");
-            for (std::size_t i = 0; i < indices.size(); ++i) {
-                if (indices[i].is_ear) {
-                    ears.push_back(i);
-                }
-            }
+        // Add triangle
+        triangles.push_back(point_info.curr_index);
+        if (is_ccw) {
+            triangles.push_back(point_info.next_index);
+            triangles.push_back(point_info.prev_index);
+        } else {
+            triangles.push_back(point_info.prev_index);
+            triangles.push_back(point_info.next_index);
         }
 
-        if (ears.empty()) {
-            throw std::runtime_error("Ears are empty. It's bug in triangulation algorithm.");
-        }
+        indices.erase(std::next(indices.begin(), ear_index));
 
-        size_t min_angle_index = ears[0];
-        {
-            [[maybe_unused]] const auto& p3 = profiler::count_scope("best");
-            min_angle_index = *(std::max_element(ears.begin(), ears.end(), [&indices](std::size_t a, std::size_t b) {
-                return indices[a].dot < indices[b].dot;
-            }));
-        }
+        const auto prev_index_in_indices = (ear_index + indices.size() - 1) % indices.size();
+        const auto next_index_in_indices = ear_index % indices.size();
 
-        {
-            [[maybe_unused]] const auto& p4 = profiler::count_scope("add");
-
-            const auto& point_info = indices[min_angle_index];
-
-            // Add triangle
-            triangles.push_back(point_info.curr_index);
-            if (is_ccw) {
-                triangles.push_back(point_info.next_index);
-                triangles.push_back(point_info.prev_index);
-            } else {
-                triangles.push_back(point_info.prev_index);
-                triangles.push_back(point_info.next_index);
-            }
-
-            indices.erase(std::next(indices.begin(), min_angle_index));
-
-            const auto prev_index_in_indices = (min_angle_index + indices.size() - 1) % indices.size();
-            const auto next_index_in_indices = min_angle_index % indices.size();
-
-            // update neighbors point infos
-            update_info(prev_index_in_indices);
-            update_info(next_index_in_indices);
-        }
+        // update neighbors point infos
+        update_info(indices, prev_index_in_indices, polygon, is_ccw);
+        update_info(indices, next_index_in_indices, polygon, is_ccw);
     }
 
     // Add last triangle
