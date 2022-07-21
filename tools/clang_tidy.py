0c1f89d0c74872e8df9e051ace397c71cb3bcc4d
alexiynew <lehafeda@gmail.com>
2022-07-21 17:40:48 +0300

    Use compilation database to run clang-tidy

--- tools/clang_tidy.py
+++ tools/clang_tidy.py
@@ -1,50 +1,14 @@
 #! /usr/bin/python3
 
 import subprocess
+import json
 import os
 
 script_path = os.getcwd()
 
-pathes = [
-    os.path.join(script_path, "neutrino/common"),
-    os.path.join(script_path, "neutrino/game_core"),
-    os.path.join(script_path, "neutrino/graphics"),
-    os.path.join(script_path, "neutrino/log"),
-    os.path.join(script_path, "neutrino/math"),
-    os.path.join(script_path, "neutrino/profiler"),
-    #os.path.join(script_path, "neutrino/system"),
-    os.path.join(script_path, "neutrino/unit_test"),
-    #os.path.join(script_path, "example"),
-    #os.path.join(script_path, "test"),
-]
-
-code_extensions = [
-    "c", "cc", "cpp", "cxx",
-    "h", "hh", "hpp", "hxx",
-    "m", "mm",
-]
-
-
-def has_extension_filter(extensions):
-    def check_extension(filename):
-        _, ext = os.path.splitext(filename)
-        return ext[1:] in extensions
-    return check_extension
-
-
-def get_files(pathes_list):
-    files = []
-
-    for p in pathes_list:
-        for (dirpath, dirnames, filenames) in os.walk(p):
-            for f in filenames:
-                files.append(os.path.join(dirpath, f))
-
-    return files
-
-
-files = list(filter(has_extension_filter(code_extensions), get_files(pathes)))
-
+# Load the database and extract all files.
+database = json.load(open(os.path.join(script_path, "build/compile_commands.json")))
+files = set([entry['file'] for entry in database])
 
 has_errors = False
 for f in files:
